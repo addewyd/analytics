17b7266a822c861bf350905ae9573a528dd1d4f6
addewyd <addewyd@mail.ru>
2018-09-28 09:29:13 +0400

    stations

---
+++ project/StationsUnit.pas
@@ -0,0 +1,77 @@
+unit StationsUnit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FormWithGridUnit, Data.DB,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
+  FireDAC.Stan.Async, FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  JvDataSource, System.ImageList, Vcl.ImgList, Vcl.Menus, System.Actions,
+  Vcl.ActnList, JvFormPlacement, JvComponentBase, JvAppStorage,
+  JvAppRegistryStorage, JvDBGridFooter, Vcl.Grids, Vcl.DBGrids, JvExDBGrids,
+  JvDBGrid, Vcl.ComCtrls, JvExComCtrls, JvStatusBar, Vcl.ToolWin, JvToolBar;
+
+type
+  TStationsForm = class(TFormWithGrid)
+    FDUpdSQL: TFDUpdateSQL;
+    UpdateAction: TAction;
+    ToolButton2: TToolButton;
+    procedure FormCreate(Sender: TObject);
+    procedure UpdateActionExecute(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  StationsForm: TStationsForm;
+
+implementation
+
+{$R *.dfm}
+
+uses DmUnit;
+
+procedure TStationsForm.FormCreate(Sender: TObject);
+begin
+  inherited;
+  with FDQuery do
+    begin
+    Transaction.StartTransaction;
+    try
+      Open;
+      FetchAll;
+      Transaction.Commit;
+    except
+      on e: exception do
+      begin
+        Transaction.Rollback;
+        ErrorMessageBox(self, e.message);
+      end;
+    end;
+  end;
+end;
+
+procedure TStationsForm.UpdateActionExecute(Sender: TObject);
+begin
+  inherited;
+  with FDQuery do
+  begin
+    Transaction.StartTransaction;
+    try
+      ApplyUpdates;
+      Transaction.Commit;
+    except
+      on e: exception do
+      begin
+        Transaction.Rollback;
+        ErrorMessageBox(self, e.message);
+      end;
+    end;
+  end;
+end;
+
+end.
